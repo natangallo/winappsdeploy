805dbd23127a4b82c4e44946c5fa6bd48b1b93e7
natangallo <39185743+natangallo@users.noreply.github.com>
2024-07-23 18:48:13 +0200

    Update setup_client_remotely.ps1

--- setup_client_remotely.ps1
+++ setup_client_remotely.ps1
@@ -1,8 +1,17 @@
+# Configurazione della rete come privata
+try {
+    $net = Get-NetConnectionProfile
+    Set-NetConnectionProfile -Name $net.Name -NetworkCategory Private
+    Write-Host "La connessione di rete è stata configurata come privata su $env:COMPUTERNAME."
+} catch {
+    Write-Host "Errore durante la configurazione della rete come privata su $env:COMPUTERNAME: $_"
+}
+
 # Abilitazione del servizio WinRM
 try {
     winrm quickconfig -q
-    winrm set winrm/config/service @{AllowUnencrypted="true"}
-    winrm set winrm/config/service/auth @{Basic="true"}
+    #winrm set winrm/config/service @{AllowUnencrypted="true"}
+    #winrm set winrm/config/service/auth @{Basic="true"}
     Write-Host "WinRM configurato correttamente su $env:COMPUTERNAME."
 } catch {
     Write-Host "Errore durante la configurazione di WinRM su $env:COMPUTERNAME: $_"
@@ -12,12 +21,15 @@ try {
 try {
     if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
         # Scarica e installa Winget
-        Invoke-WebRequest -Uri "https://aka.ms/get-winget" -OutFile "$env:USERPROFILE\Downloads\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle"
-        Add-AppxPackage -Path "$env:USERPROFILE\Downloads\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle"
+        Invoke-WebRequest -Uri "https://aka.ms/get-winget" -OutFile "$env:USERPROFILE\Downloads\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+        Add-AppxPackage -Path "$env:USERPROFILE\Downloads\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
         Write-Host "Winget installato correttamente su $env:COMPUTERNAME."
     } else {
-        Write-Host "Winget è già installato su $env:COMPUTERNAME."
+        # Aggiornamento di Winget all'ultima versione
+        Invoke-WebRequest -Uri "https://aka.ms/get-winget" -OutFile "$env:USERPROFILE\Downloads\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+        Add-AppxPackage -Path "$env:USERPROFILE\Downloads\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+        Write-Host "Winget aggiornato correttamente su $env:COMPUTERNAME."
     }
 } catch {
-    Write-Host "Errore durante l'installazione di Winget su $env:COMPUTERNAME: $_"
+    Write-Host "Errore durante l'installazione/aggiornamento di Winget su $env:COMPUTERNAME: $_"
 }
