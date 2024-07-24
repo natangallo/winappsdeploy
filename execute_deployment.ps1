5816ebdbdc24f4bd96e3efc8250f8c6d4678c51c
natangallo <39185743+natangallo@users.noreply.github.com>
2024-07-24 12:46:06 +0200

    Update execute_deployment.ps1

--- execute_deployment.ps1
+++ execute_deployment.ps1
@@ -16,12 +16,27 @@ $username = Read-Host "Inserisci il nome utente amministratore"
 $password = Read-Host "Inserisci la password amministratore" -AsSecureString
 $credential = New-Object System.Management.Automation.PSCredential ($username, $password)
 
-# Verifica e creazione della directory dei log
+# Ottenere l'indirizzo IP del computer di amministrazione
+# Verrà utilizzato per trasferire i log di installazione dei client
+# Se si è valutato di spostare i log su un file server esterno, considerare di attivare la seguente variabile e commentare la seccessiva
+# Aggiornare quindi sia la variabile $remoteIPAddress, sia la variabile $fileRoot
+
+# $remoteIPAddress = "\\$fileRoot"
+$remoteIPAddress = (Get-NetIPAddress | Where-Object { $_.AddressFamily -eq 'IPv4' -and $_.InterfaceAlias -ne 'Loopback' } | Select-Object -First 1).IPAddress
+$adminIpAddress = "\\$remoteIPAddress"
+
+# Verifica e creazione della directory dei log e software locali
 $fileRoot = "C:" # "\\serveriporname" può esssere modificato in caso di cartella di rete esterna.
 $logsDirectory = "$fileRoot\logs"
 if (-not (Test-Path -Path $logsDirectory)) {
     New-Item -Path $logsDirectory -ItemType Directory
 }
+
+$msiDirectory = "$fileRoot\MSIFiles"
+if (-not (Test-Path -Path $logsDirectory)) {
+    New-Item -Path $logsDirectory -ItemType Directory
+}
+$rootFolder = "$adminIpAddress\MSIFiles"
 
 # File di log sul computer di amministrazione
 $adminLogFile = "$logsDirectory\admin_logfile.txt"
@@ -32,12 +47,7 @@ $adminLogFile = "$logsDirectory\admin_logfile.txt"
 $clientsFile = "$fileRoot\clients.txt"
 $appsFile = "$fileRoot\apps.txt"
 
-# Ottenere l'indirizzo IP del computer di amministrazione
-# Verrà utilizzato per trasferire i log di installazione dei client
-# Se si è valutato di spostare i log su un file server esterno, considerare di attivare la seguente variabile e commentare la seccessiva
-# $adminIpAddress = "\\$fileRoot"
-$remoteIPAddress = (Get-NetIPAddress | Where-Object { $_.AddressFamily -eq 'IPv4' -and $_.InterfaceAlias -ne 'Loopback' } | Select-Object -First 1).IPAddress
-$adminIpAddress = "\\$remoteIPAddress"
+
 
 if (Test-Path -Path $clientsFile) {
     $clients = Get-Content -Path $clientsFile
@@ -61,23 +71,20 @@ if (Test-Path -Path $appsFile) {
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
 #########################################################################################################
 
-$scriptBlock = {
+# Funzione per l'installazione di applicazioni tramite Winget
+$scriptBlockWinget = {
     param ($apps, $adminIpAddress, $credential)
     
-    # Verifica e creazione della directory dei log locali
     $remoteLogFile = "C:\logfile.txt"
-    "" | Out-File -FilePath $remoteLogFile -Append
     "Log iniziale sul client remoto - $(Get-Date)" | Out-File -FilePath $remoteLogFile -Append
 
     foreach ($app in $apps) {
         try {
             $appInfo = winget list | Where-Object { $_ -match $app }
             if ($appInfo) {
-                # Se l'applicazione è già installata, aggiorna all'ultima versione
                 winget upgrade --id $app --silent --accept-package-agreements --accept-source-agreements
                 "$app aggiornato correttamente su $env:COMPUTERNAME - $(Get-Date)" | Out-File -FilePath $remoteLogFile -Append
             } else {
-                # Se l'applicazione non è installata, procedi con l'installazione
                 winget install --id $app --silent --accept-package-agreements --accept-source-agreements
                 "$app installato correttamente su $env:COMPUTERNAME - $(Get-Date)" | Out-File -FilePath $remoteLogFile -Append
             }
@@ -86,33 +93,69 @@ $scriptBlock = {
         }
     }
 
-    # Trasferimento del file di log al computer di amministrazione
-    $adminLogsPath = "$adminIpAddress\logs\"
+    $adminLogsPath = "$adminIpAddress\logs\${env:COMPUTERNAME}_logfile.txt"
     try {
-        New-PSDrive -Name "Z" -PSProvider FileSystem -Root $adminLogsPath -Credential $credential -ErrorAction Stop
-        Copy-Item -Path $remoteLogFile -Destination Z:\${env:COMPUTERNAME}_logfile.txt
+        Copy-Item -Path $remoteLogFile -Destination $adminLogsPath
         "Log trasferito correttamente a $adminLogsPath - $(Get-Date)" | Out-File -FilePath $remoteLogFile -Append
     } catch {
         "Errore durante il trasferimento del log a $adminLogsPath - $(Get-Date): $_" | Out-File -FilePath $remoteLogFile -Append
     }
 }
 
-# Verifica dei requisiti sui client remoti
-$parametersVerify = @{
-    ComputerName          = $clients
-    InDisconnectedSession = $true
-    FilePath              = "$fileRoot\verify_requirements.ps1"
-    Credential            = $credential
-}
-Invoke-Command @parametersVerify
+# Funzione per l'installazione di file MSI
+$scriptBlockMsi = {
+    param ($apps, $adminIpAddress, $credential)
+    
+    $remoteLogFile = "C:\logfile.txt"
+    "Log iniziale sul client remoto - $(Get-Date)" | Out-File -FilePath $remoteLogFile -Append
 
-# Connessione a ciascun client e esecuzione dello script di setup e installazione/aggiornamento
-$parametersExecute = @{
-    ComputerName          = $clients
-    ScriptBlock           = $scriptBlock
-    ArgumentList          = $apps, $adminIpAddress, $credential
-    Credential            = $credential
+    foreach ($app in $apps) {
+        try {
+            $msiPath = Join-Path -Path $rootFolder -ChildPath $app
+            if (Test-Path -Path $msiPath) {
+                Start-Process msiexec.exe -ArgumentList "/i `"$msiPath`" /quiet /norestart" -Wait
+                "$app installato correttamente su $env:COMPUTERNAME - $(Get-Date)" | Out-File -FilePath $remoteLogFile -Append
+            } else {
+                "File MSI non trovato: $msiPath - $(Get-Date)" | Out-File -FilePath $remoteLogFile -Append
+            }
+        } catch {
+            "Errore durante l'installazione di $app su $env:COMPUTERNAME - $(Get-Date): $_" | Out-File -FilePath $remoteLogFile -Append
+        }
+    }
+
+    $adminLogsPath = "$adminIpAddress\logs\${env:COMPUTERNAME}_logfile.txt"
+    try {
+        Copy-Item -Path $remoteLogFile -Destination $adminLogsPath
+        "Log trasferito correttamente a $adminLogsPath - $(Get-Date)" | Out-File -FilePath $remoteLogFile -Append
+    } catch {
+        "Errore durante il trasferimento del log a $adminLogsPath - $(Get-Date): $_" | Out-File -FilePath $remoteLogFile -Append
+    }
 }
+
+# Richiesta della tipologia di installazione
+$installType = Read-Host "Inserisci la tipologia di installazione (Winget/MSI)"
+
+# Esecuzione delle installazioni in base alla tipologia
+if ($installType -eq 'Winget') {
+    $parametersExecute = @{
+        ComputerName          = $clients
+        ScriptBlock           = $scriptBlockWinget
+        ArgumentList          = $apps, $adminIpAddress, $credential
+        Credential            = $credential
+    }
+} elseif ($installType -eq 'MSI') {
+    $parametersExecute = @{
+        ComputerName          = $clients
+        ScriptBlock           = $scriptBlockMsi
+        ArgumentList          = $apps, $adminIpAddress, $credential
+        Credential            = $credential
+    }
+} else {
+    "Tipologia di installazione non valida. Esci." | Out-File -FilePath $adminLogFile -Append
+    exit 1
+}
+
+# Esecuzione dello script
 $results = Invoke-Command @parametersExecute
 
 "Script di distribuzione completato - $(Get-Date)" | Out-File -FilePath $adminLogFile -Append
